ca0dc2cbceec237cf94be3858e5355c9d5cd2a2c
Makarov Andrii <makarov.andrii.10@gmail.com>
2023-09-21 14:18:38 +0300

    add Palindrome

---
+++ src/main/java/org/makarov/lab4_1/Palindrome.java
@@ -0,0 +1,16 @@
+package org.makarov.lab4_1;
+
+public class Palindrome {
+    public static boolean isPalindrome(String string) {
+        var input = string.trim().toUpperCase();
+        var length = input.length();
+
+        for (int i = 0; i < Math.ceil(length / 2.0); i++) {
+            if (input.charAt(i) != input.charAt(length - i - 1)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
