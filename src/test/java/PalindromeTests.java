88282396212579feb356cee835ecb2ad861edaf6
Makarov Andrii <makarov.andrii.10@gmail.com>
2023-09-21 14:17:57 +0300

    add tests

---
+++ src/test/java/PalindromeTests.java
@@ -0,0 +1,24 @@
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.makarov.lab4_1.Palindrome.isPalindrome;
+
+public class PalindromeTests {
+    @Test
+    public void testPalindrome() {
+        assertTrue(isPalindrome("level"));
+        assertFalse(isPalindrome("not"));
+    }
+
+    @Test
+    public void testIgnoreSpaces() {
+        assertTrue(isPalindrome(" l e v e l "));
+    }
+
+    @Test
+    public void testIgnoreRegister() {
+        assertTrue(isPalindrome("LEvel"));
+    }
+}
