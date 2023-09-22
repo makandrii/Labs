9a7a09d0e8858fdefdb4d032e79e7b2b0ee2b5ba
Makarov Andrii <makarov.andrii.10@gmail.com>
2023-09-22 19:15:12 +0300

    add functionality

--- src/main/java/org/makarov/lab5/BankAccount.java
+++ src/main/java/org/makarov/lab5/BankAccount.java
@@ -1,7 +1,28 @@
 package org.makarov.lab5;
 
 public class BankAccount {
+    private static int amountOfAccounts = 0;
     private int accountNumber;
     private String accountName;
     private double balance;
+
+    public BankAccount(String name) {
+        accountNumber = ++amountOfAccounts;
+        accountName = name;
+        balance = 0.0d;
+    }
+
+    public void deposit(double amount) {
+    }
+
+    public void withdraw(double amount) {
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getAccountSummery() {
+        return String.format("№: %d | Name: %s | Balance: %f", accountNumber, accountName, balance);
+    }
 }
