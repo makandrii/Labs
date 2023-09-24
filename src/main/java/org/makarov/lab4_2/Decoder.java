ec6f2eca95fef7149526eb7f3f37615e4d2dbcdd
Makarov Andrii <makarov.andrii.10@gmail.com>
2023-09-24 18:36:50 +0300

    add Decoder

---
+++ src/main/java/org/makarov/lab4_2/Decoder.java
@@ -0,0 +1,37 @@
+package org.makarov.lab4_2;
+
+public class Decoder {
+    public static String vowelsMethodDecode(String word) {
+        return word.chars()
+                .mapToObj(symbol -> {
+                    switch (symbol) {
+                        case '1':
+                            return 'a';
+                        case '2':
+                            return 'e';
+                        case '3':
+                            return 'i';
+                        case '4':
+                            return 'o';
+                        case '5':
+                            return 'u';
+                        default:
+                            return (char) symbol;
+                    }
+                })
+                .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
+                .toString();
+    }
+
+    public static String consonantsMethodDecode(String word) {
+        return word.chars().map(symbol -> {
+            if ("bfjpv".contains(Character.toString(symbol))) {
+                return symbol - 2;
+            }
+            if ("cdghklmnqrstwxyz".contains(Character.toString(symbol))) {
+                return symbol - 1;
+            }
+            return symbol;
+        }).collect(StringBuilder::new, StringBuilder::append, StringBuilder::append).toString();
+    }
+}
