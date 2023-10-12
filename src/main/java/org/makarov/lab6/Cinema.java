73835947fa3cd7db528b4ad782135b59c5aae66d
Makarov Andrii <makarov.andrii.10@gmail.com>
2023-10-12 13:34:10 +0300

    add printSeatingArrangement(int hallNumber)

--- src/main/java/org/makarov/lab6/Cinema.java
+++ src/main/java/org/makarov/lab6/Cinema.java
@@ -11,7 +11,7 @@ public class Cinema {
     }
 
     public void bookSeats(int hallNumber, int row, int[] seats) {
-        if (!isBooked(hallNumber, row, seats)) {
+        if (isBooked(hallNumber, row, seats)) {
             System.err.println("One or more of these seats are already booked");
             return;
         }
@@ -30,6 +30,25 @@ public class Cinema {
                                 .anyMatch(secondPtr -> cinema[screen][row][firstPtr + secondPtr] == 1)
                         )
                 );
+    }
+
+    public void printSeatingArrangement(int hallNumber) {
+        final String RESET = "\u001B[0m";
+        final String BLACK_BG = "\u001B[40m\u001B[32m";
+        final String YELLOW_BG = "\u001B[43m\u001B[31m";
+
+        String header = "\t\t1\t2\t3\t4\t5\t6\t7\t8\t9  10  11  12  13  14  15  16  17  18  19  20\t\t\n";
+        StringBuilder output = new StringBuilder(header);
+        for (int row = 0; row < 10; row++) {
+            output.append((row + 1 >= 10) ? " " : "  ").append(String.format("%d |%s", row + 1, BLACK_BG));
+            for (var seat : cinema[hallNumber][row]) {
+                output.append(seat == 0? String.format("\t%s0", BLACK_BG)
+                        : String.format("%s\t1", YELLOW_BG));
+            }
+            output.append(String.format("\t%s| %d  \n", RESET, row + 1));
+        }
+        output.append(header);
+        System.out.println(output);
     }
 
     private boolean isBooked(int hallNumber, int row, int[] seats) {
