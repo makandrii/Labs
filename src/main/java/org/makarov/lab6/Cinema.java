92fc67aafe4d7be14d476cea43ae2cd4bf97eff2
Makarov Andrii <makarov.andrii.10@gmail.com>
2023-10-12 12:12:26 +0300

    add checkAvailability(int screen, int numSeats)

--- src/main/java/org/makarov/lab6/Cinema.java
+++ src/main/java/org/makarov/lab6/Cinema.java
@@ -1,6 +1,7 @@
 package org.makarov.lab6;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class Cinema {
     public int[][][] cinema;
@@ -20,6 +21,15 @@ public class Cinema {
 
     public void cancelBooking(int hallNumber, int row, int[] seats) {
         Arrays.stream(seats).forEach(seat -> cinema[hallNumber][row][seat] = 0);
+    }
+
+    public boolean checkAvailability(int screen, int numSeats) {
+        return IntStream.range(0, 10)
+                .allMatch(row -> IntStream.range(0, 20 - numSeats)
+                        .noneMatch(firstPtr -> IntStream.range(0, numSeats)
+                                .anyMatch(secondPtr -> cinema[screen][row][firstPtr + secondPtr] == 1)
+                        )
+                );
     }
 
     private boolean isBooked(int hallNumber, int row, int[] seats) {
