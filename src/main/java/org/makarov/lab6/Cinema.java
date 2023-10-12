e66b7231c5cca4bcbfba84ea307b117c96503cf2
Makarov Andrii <makarov.andrii.10@gmail.com>
2023-10-12 11:25:16 +0300

    add bookSeats(int hallNumber, int row, int[] seats)

--- src/main/java/org/makarov/lab6/Cinema.java
+++ src/main/java/org/makarov/lab6/Cinema.java
@@ -8,4 +8,18 @@ public class Cinema {
     public Cinema() {
         cinema = new int[5][10][20];
     }
+
+    public void bookSeats(int hallNumber, int row, int[] seats) {
+        if (!isBooked(hallNumber, row, seats)) {
+            System.err.println("One or more of these seats are already booked");
+            return;
+        }
+
+        Arrays.stream(seats).forEach(seat -> cinema[hallNumber][row][seat] = 1);
+    }
+
+    private boolean isBooked(int hallNumber, int row, int[] seats) {
+        return Arrays.stream(seats)
+                .anyMatch(seat -> cinema[hallNumber][row][seat] == 1);
+    }
 }
