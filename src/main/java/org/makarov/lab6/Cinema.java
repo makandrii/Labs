918f83b269c805875220218859f097a9fa9da994
Makarov Andrii <makarov.andrii.10@gmail.com>
2023-10-12 11:15:52 +0300

    add Cinema

---
+++ src/main/java/org/makarov/lab6/Cinema.java
@@ -0,0 +1,11 @@
+package org.makarov.lab6;
+
+import java.util.Arrays;
+
+public class Cinema {
+    public int[][][] cinema;
+
+    public Cinema() {
+        cinema = new int[5][10][20];
+    }
+}
