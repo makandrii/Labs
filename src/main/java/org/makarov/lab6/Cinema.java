b0b40fae6c4b03517fad3bb5412ca56560a618cc
Makarov Andrii <makarov.andrii.10@gmail.com>
2023-10-14 11:25:28 +0300

    add findBestAvailable(int hallNumber, int numSeats)

--- src/main/java/org/makarov/lab6/Cinema.java
+++ src/main/java/org/makarov/lab6/Cinema.java
@@ -1,6 +1,8 @@
 package org.makarov.lab6;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 public class Cinema {
@@ -24,8 +26,8 @@ public class Cinema {
     }
 
     public boolean checkAvailability(int screen, int numSeats) {
-        return IntStream.range(0, 10)
-                .allMatch(row -> IntStream.range(0, 20 - numSeats)
+        return IntStream.range(0, cinema[screen].length)
+                .allMatch(row -> IntStream.range(0, cinema[screen][row].length - numSeats)
                         .noneMatch(firstPtr -> IntStream.range(0, numSeats)
                                 .anyMatch(secondPtr -> cinema[screen][row][firstPtr + secondPtr] == 1)
                         )
@@ -39,10 +41,10 @@ public class Cinema {
 
         String header = "\t\t1\t2\t3\t4\t5\t6\t7\t8\t9  10  11  12  13  14  15  16  17  18  19  20\t\t\n";
         StringBuilder output = new StringBuilder(header);
-        for (int row = 0; row < 10; row++) {
+        for (int row = 0; row < cinema[hallNumber].length; row++) {
             output.append((row + 1 >= 10) ? " " : "  ").append(String.format("%d |%s", row + 1, BLACK_BG));
             for (var seat : cinema[hallNumber][row]) {
-                output.append(seat == 0? String.format("\t%s0", BLACK_BG)
+                output.append(seat == 0 ? String.format("\t%s0", BLACK_BG)
                         : String.format("%s\t1", YELLOW_BG));
             }
             output.append(String.format("\t%s| %d  \n", RESET, row + 1));
@@ -51,8 +53,65 @@ public class Cinema {
         System.out.println(output);
     }
 
+    public Optional<int[]> findBestAvailable(int hallNumber, int numSeats) {
+        if (numSeats > cinema[hallNumber][0].length) {
+            return Optional.empty();
+        }
+
+        Optional<Integer>[][] costs = buildArrayOfCosts(hallNumber);
+        for (int row = 0; row < cinema[hallNumber].length; row++) {
+            for (int seat = 0; seat < cinema[hallNumber][row].length; seat++) {
+                if (cinema[hallNumber][row][seat] == 1) {
+                    costs[row][seat] = Optional.empty();
+                }
+            }
+        }
+
+        int[] result = new int[0];
+        int maxSum = 0;
+        for (int row = 0; row < cinema[hallNumber].length; row++) {
+            for (int firstPtr = 0; firstPtr < cinema[hallNumber][row].length - numSeats; firstPtr++) {
+                int currentSum = 0;
+                for (int secondPtr = 0, i = 0; secondPtr < numSeats; secondPtr++) {
+                    if (costs[row][firstPtr + secondPtr].isPresent()) {
+                        currentSum += costs[row][firstPtr + secondPtr].get();
+                    } else {
+                        firstPtr += secondPtr;
+                        break;
+                    }
+                }
+                if (currentSum > maxSum) {
+                    maxSum = currentSum;
+                    result = new int[2];
+                    result[0] = row;
+                    result[1] = firstPtr;
+                }
+            }
+        }
+
+        if (result.length == 0) {
+            return Optional.empty();
+        } else {
+            return Optional.of(result);
+        }
+    }
+
     private boolean isBooked(int hallNumber, int row, int[] seats) {
         return Arrays.stream(seats)
                 .anyMatch(seat -> cinema[hallNumber][row][seat] == 1);
+    }
+
+    private Optional<Integer>[][] buildArrayOfCosts(int hallNumber) {
+        Optional<Integer>[][] result = new Optional[cinema[hallNumber].length][cinema[hallNumber][0].length];
+        for (int row = 0; row < Math.ceil(cinema[hallNumber].length / 2.0); row++) {
+            for (int seat = 0, cost = row; seat < Math.ceil(result[row].length / 2.0); seat++, cost++) {
+                result[row][seat]
+                        = result[row][result[row].length - 1 - seat]
+                        = result[result.length - 1 - row][seat]
+                        = result[result.length - 1 - row][result[row].length - 1 - seat]
+                        = Optional.of(cost);
+            }
+        }
+        return result;
     }
 }
