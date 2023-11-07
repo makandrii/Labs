94f28bc6a27b0546093f743222f62605cf292a24
Makarov Andrii <makarov.andrii.10@gmail.com>
2023-11-07 14:32:56 +0200

    change Product

--- src/main/java/org/makarov/lab7/Product.java
+++ src/main/java/org/makarov/lab7/Product.java
@@ -1,11 +1,24 @@
 package org.makarov.lab7;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
-@Data
+@Getter
+@ToString
 public class Product {
-    private Integer id;
-    private String name;
+    private final Integer id;
+    private final String name;
+    @Setter
     private double price;
+    @Setter
     private int stock;
+    private static Integer counter = 0;
+
+    public Product(String name, double price, int stock) {
+        id = ++counter;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
 }
