19bab047c6c4fb8eade844f5f2081cd9ac47fbec
Makarov Andrii <makarov.andrii.10@gmail.com>
2023-11-07 14:44:28 +0200

    change Order

--- src/main/java/org/makarov/lab7/Order.java
+++ src/main/java/org/makarov/lab7/Order.java
@@ -1,18 +1,26 @@
 package org.makarov.lab7;
 
-import lombok.Data;
+import lombok.Getter;
 
 import java.util.Map;
 
-@Data
+@Getter
 public class Order {
-    private Integer id;
-    private Integer userId;
-    private Map<Product, Integer> orderDetails;
+    private final Integer id;
+    private final Integer userId;
+    private final Map<Product, Integer> orderDetails;
     private double totalPrice;
+    private static Integer counter = 0;
 
-    public double calculateTotalPrice() {
-        return totalPrice = orderDetails.entrySet().stream().reduce(0.0, (sum, entry) ->
+    public Order(Integer userId, Map<Product, Integer> orderDetails) {
+        id = ++counter;
+        this.userId = userId;
+        this.orderDetails = orderDetails;
+        calculateTotalPrice();
+    }
+
+    public void calculateTotalPrice() {
+        totalPrice = orderDetails.entrySet().stream().reduce(0.0, (sum, entry) ->
                 sum + entry.getKey().getPrice() * entry.getValue(), Double::sum);
     }
 }
