091272b4b60bddf643f9ccc2b46852bf058308b1
Makarov Andrii <makarov.andrii.10@gmail.com>
2023-11-07 17:56:07 +0200

    add functionality for ECommercePlatform

--- src/main/java/org/makarov/lab7/ECommercePlatform.java
+++ src/main/java/org/makarov/lab7/ECommercePlatform.java
@@ -1,12 +1,42 @@
 package org.makarov.lab7;
 
-import lombok.Data;
-
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-@Data
 public class ECommercePlatform {
-    private Map<Integer, User> users;
-    private Map<Integer, Product> products;
-    private Map<Integer, Order> orders;
+    private final Map<Integer, User> users = new HashMap<>();
+    private final Map<Integer, Product> products = new HashMap<>();
+    private final Map<Integer, Order> orders = new HashMap<>();
+
+    public void addUser(User user) {
+        users.put(user.getId(), user);
+    }
+
+    public void addProduct(Product product) {
+        products.put(product.getId(), product);
+    }
+
+    public void makeOrder(Integer userId) {
+        User user = users.get(userId);
+        Order order = new Order(user.getId(), user.getCart());
+        orders.put(order.getId(), order);
+    }
+
+    public List<Product> makeListOfAvailableProducts() {
+        return products.values().stream().filter(product -> product.getStock() > 0).toList();
+    }
+
+    public List<User> makeListOfUsers() {
+        return users.values().stream().toList();
+    }
+
+    public List<Order> makeListOfOrders() {
+        return orders.values().stream().toList();
+    }
+
+    public void updateStock(Integer productId, int newStock) {
+        Product product = products.get(productId);
+        product.setStock(newStock);
+    }
 }
