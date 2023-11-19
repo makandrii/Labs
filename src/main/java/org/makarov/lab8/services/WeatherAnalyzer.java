7b8dc2650b21ca00c702bbc23c51c8acd0e617e3
Makarov Andrii <makarov.andrii.10@gmail.com>
2023-11-19 15:49:43 +0200

    rename old methods, add new methods

--- src/main/java/org/makarov/lab8/services/WeatherAnalyzer.java
+++ src/main/java/org/makarov/lab8/services/WeatherAnalyzer.java
@@ -1,33 +1,77 @@
 package org.makarov.lab8.services;
 
+import org.makarov.lab8.models.City;
 import org.makarov.lab8.models.Day;
 import org.makarov.lab8.models.Station;
 import org.makarov.lab8.models.WeatherData;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class WeatherAnalyzer {
-    private static final Comparator<Day> byTemperatureComparator
-            = Comparator.comparingDouble(Day::getTemperature);
+    private static final Comparator<Day> byTemperatureComparator = Comparator.comparingDouble(Day::getTemperature);
+    private static final Comparator<Day> byHumidityComparator = Comparator.comparingDouble(Day::getHumidity);
 
-    public static List<Station> findHottestStations(int count, WeatherData data) {
-        return findStationsSortedBy(count, data, byTemperatureComparator.reversed());
+    public static List<Station> findHottest(int maxCount, WeatherData data) {
+        return sortStationsBy(maxCount, data, byTemperatureComparator.reversed());
     }
 
-    public static List<Station> findColdestStations(int count, WeatherData data) {
-        return findStationsSortedBy(count, data, byTemperatureComparator);
+    public static List<Station> findColdest(int maxCount, WeatherData data) {
+        return sortStationsBy(maxCount, data, byTemperatureComparator);
     }
 
-    private static List<Station> findStationsSortedBy(int count, WeatherData data, Comparator<Day> comparator) {
-        return data.getLocations().stream()
+    public static List<Station> findHighestHumidity(int maxCount, WeatherData data) {
+        return sortStationsBy(maxCount, data, byHumidityComparator.reversed());
+    }
+
+    public static List<Station> findWithPrecipitation(int countOfDays, WeatherData data) {
+        return normalizeData(data)
+                .filter(city -> {
+                    List<Day> days = city.getDays();
+                    first: for (int firstPtr = 0; firstPtr < days.size() - countOfDays + 1; firstPtr++) {
+                        for (int secondPtr = 0; secondPtr < countOfDays; secondPtr++) {
+                            if (days.get(firstPtr + secondPtr).getPrecipitation() == 0) {
+                                continue first;
+                            }
+                        }
+                        return true;
+                    }
+                    return false;
+                })
+                .flatMap(city -> city.getStations().values().stream())
+                .toList();
+    }
+
+    public static List<Station> findWithIncreasedTemperature(int delta, int countOfDays, WeatherData data) {
+        return normalizeData(data)
+                .filter(city -> {
+                    List<Day> days = city.getDays();
+                    for (int firstPtr = 0; firstPtr < days.size() - countOfDays + 1; firstPtr++) {
+                        for (int secondPtr = 0; secondPtr < countOfDays; secondPtr++) {
+                            if (days.get(secondPtr).getTemperature() - days.get(firstPtr).getTemperature() >= delta) {
+                                return true;
+                            }
+                        }
+                    }
+                    return false;
+                })
+                .flatMap(city -> city.getStations().values().stream())
+                .toList();
+    }
+
+    private static List<Station> sortStationsBy(int maxCount, WeatherData data, Comparator<Day> comparator) {
+        return normalizeData(data)
                 .sorted(Comparator.comparing(
                         c -> c.getDays().get(c.getDays().size() - 1),
                         comparator))
-                .filter(city -> city.getStations() != null)
                 .flatMap(city -> city.getStations().values().stream())
-                .limit(count)
-                .collect(Collectors.toList());
+                .limit(maxCount)
+                .toList();
+    }
+
+    private static Stream<City> normalizeData(WeatherData data) {
+        return data.getLocations().stream()
+                .filter(city -> city.getStations() != null);
     }
 }
